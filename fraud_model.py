e78abbef4bd1d0935ad5841d711721412815b50b
Darkwolf1268 <avnigautam.84@gmail.com>
2026-03-11 00:57:08 +0530

    Update fraud_model.py

--- fraud_model.py
+++ fraud_model.py
@@ -1,1 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
+data = pd.read_csv("dataset/insurance_fraud.csv")
+
+data = data.dropna()
+
+X = data.drop("fraud_reported",axis=1)
+y = data["fraud_reported"]
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+model = RandomForestClassifier()
+
+model.fit(X_train,y_train)
+
+pickle.dump(model,open("model/model.pkl","wb"))
+
+print("Model trained successfully")
